0236e9e0d6db4e600a29fda4868875c3b89188e5
mahajrod <mahajrod@gmail.com>
2021-03-21 01:57:42 +0700

    added support of gziped and bziped files

--- workflow/scripts/combine_same_sample_vcf.py
+++ workflow/scripts/combine_same_sample_vcf.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
+import bz2
+import gzip
+import sys
 
 import argparse
 
 from pandas import read_csv
+
+
+if sys.version_info[0] == 3:
+    from io import TextIOWrapper as file
+
+
+def metaopen(filename, flags, buffering=None):
+    if not isinstance(filename, str):  # or isinstance(filename, gzip.GzipFile) or isinstance(filename, bz2.BZ2File):
+        if isinstance(filename, file):
+            return filename
+        else:
+            raise ValueError("ERROR!!! Not file object or str: {}".format(str(filename)))
+    elif filename[-3:] == ".gz":
+        return gzip.open(filename, flags + ("t" if "b" not in flags else ""))
+    elif filename[-4:] == ".bz2":
+        return bz2.open(filename, flags + ("t" if "b" not in flags else ""))
+    else:
+        if buffering is not None:
+            return open(filename, flags, buffering=buffering)
+        else:
+            return open(filename, flags)
 
 
 parser = argparse.ArgumentParser()
@@ -17,16 +41,16 @@ parser.add_argument("-o", "--output", action="store", dest="output",
 args = parser.parse_args()
 
 print("Handling file %s ..." % args.path_file.iloc[0])
-with open(args.output, "w") as out_fd:
+with metaopen(args.output, "w") as out_fd:
 
-    with open(args.path_file.iloc[0], "r") as in_fd:
+    with metaopen(args.path_file.iloc[0], "r") as in_fd:
         for line in in_fd:
             out_fd.write(line)
 
     if len(args.path_file) > 1:
-        for filename in args.path_file.iloc[1:, ]:
-            print("Handling file %s ..." % filename)
-            with open(filename, "r") as in_fd:
+        for entry in args.path_file.iloc[1:, ]:
+            print("Handling file %s ..." % entry)
+            with metaopen(entry, "r") as in_fd:
                 for line in in_fd:
                     if line[0] == "#":
                         continue
